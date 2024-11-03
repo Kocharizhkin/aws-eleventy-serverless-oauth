f6a4f49a5493a71c513157694b15772a79f6e77f
Егор Кочанов <e.kochanov@aamsystems.ru>
2024-11-03 17:04:55 +0200

    changes the codebase to work locally

--- serverless/edge/s3.js
+++ serverless/edge/s3.js
@@ -1,5 +1,12 @@
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+require('dotenv').config();
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  endpoint: process.env.AWS_ENDPOINT,
+  s3ForcePathStyle: true,
+  region: 'us-east-1'
+});
 
 const fetchFileFromS3 = async (filename) => {
   const params = {
